dd6e3cb1971effb6e7b7c2b65dbe902bb00f347d
Henzard Kruger <henzard.kruger@iotnxt.com>
2022-06-08 11:28:05 +0200

    Testing all Tax types

---
+++ Payspace.Test.Project.Tests/UnitTest1.cs
@@ -0,0 +1,96 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Payspace.Test.Project.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    
+
+    [TestCase("7441", ExpectedResult = "Progressive")]
+    [TestCase("A100", ExpectedResult = "Flat Value")]
+    [TestCase("7000", ExpectedResult = "Flat rate")]
+    [TestCase("1000", ExpectedResult = "Progressive")]
+    [TestCase("0000", ExpectedResult = "Postal Code Not Setup")]
+    public string WhenPostalCodeThenGetTaxCalculation(string postalCode)
+    {
+        Assert.That(!string.IsNullOrWhiteSpace(postalCode), Is.True);
+        return postalCode switch
+        {
+            "7441" => "Progressive",
+            "A100" => "Flat Value",
+            "7000" => "Flat rate",
+            "1000" => "Progressive",
+            _ => "Postal Code Not Setup"
+        };
+    }
+
+    [TestCase(2, ExpectedResult = 0.3)]
+    [TestCase(10_001, ExpectedResult = 1500.1499999999999)]
+    [TestCase(200_000, ExpectedResult = 10_000)]
+    [TestCase(400_000, ExpectedResult = 10_000)]
+    public double WhenFlatValueThenCalculateAmount(double amount)
+    {
+        //Amount must be positive and not 0
+        Assert.That(amount, Is.GreaterThan(0));
+        if (amount >= 200_000)
+        {
+            return 10_000;
+        }
+
+        return amount * .15;
+    }
+
+    [TestCase(2, ExpectedResult = 0.34999999999999998)]
+    [TestCase(10_001, ExpectedResult = 1_750.175)]
+    [TestCase(200_000, ExpectedResult = 35_000.0)]
+    [TestCase(400_000, ExpectedResult = 70_000.0)]
+    public double WhenFlatRateThenCalculateAmount(double amount)
+    {
+        //Amount must be positive and not 0
+        Assert.That(amount, Is.GreaterThan(0));
+        return amount * .175;
+    }
+    
+    [TestCase(8_350, ExpectedResult = 835)]
+    [TestCase(33_950, ExpectedResult = 4_675.0)]
+    [TestCase(82_250, ExpectedResult = 18_002.5)]
+    [TestCase(171_550, ExpectedResult = 51_494.0)]
+    [TestCase(372_950, ExpectedResult = 140_986.0)]
+    [TestCase(400_000, ExpectedResult = 207_065.0)]
+    public double WhenProgressiveDynamicThenCalculateAmount(double amount)
+    {
+        //Amount must be positive and not 0
+        Assert.That(amount, Is.GreaterThan(0));
+        var brackets = new Dictionary<double, double>
+        {
+            { 8350, .1 },
+            { 33950, .15 },
+            { 82250, .25 },
+            { 171550, .28 },
+            { 372950, .33 },
+            { int.MaxValue, .35 }
+        };
+        return CalcProgressive(amount, brackets);
+    }
+
+    private static double CalcProgressive(double amount, Dictionary<double, double> brackets)
+    {
+        Assert.That(brackets, Is.Not.Empty);
+        var taxAmount = 0D;
+        var prevBracket = 0D;
+        foreach (var (bracket, percentage) in brackets)
+        {
+            if (amount <= bracket)
+                return taxAmount + (amount - prevBracket) * percentage;
+            taxAmount += bracket * percentage;
+            prevBracket = bracket;
+        }
+
+        return taxAmount;
+    }
+}
