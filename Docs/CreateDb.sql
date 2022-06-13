938f2a2e5bd3da2ed1adf15ce908d542ff19bce8
Henzard Kruger <henzard.kruger@iotnxt.com>
2022-06-13 16:31:21 +0200

    Update SQL File

--- Docs/CreateDb.sql
+++ Docs/CreateDb.sql
@@ -1,3 +1,9 @@
+USE [PaySpace]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_Transactions]') AND type in (N'U'))
+    DROP TABLE [dbo].[tbl_Transactions]
+GO
+IF Not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_Transactions]') AND type in (N'U'))
 create table tbl_Transactions
 (
     Id         nvarchar(250),
@@ -7,15 +13,22 @@ create table tbl_Transactions
     UserName   nvarchar(100),
     TransactionDate       DATETIME
 )
-
-create procedure sp_Transactions_Add @Id nvarchar(100),
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_Transactions_Add]'))
+DROP PROCEDURE [dbo].[sp_Transactions_Add]
+GO
+IF Not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_Transactions_Add]'))
+BEGIN
+EXEC ('CREATE PROCEDURE [dbo].[sp_Transactions_Add] ( @Id nvarchar(100),
                                      @PostalCode nvarchar(100),
                                      @Amount nvarchar(100),
                                      @Result numeric,
                                      @UserName nvarchar(50),
-                                     @TransactionDate DATETIME
+                                     @TransactionDate DATETIME)
 AS
-BEGIN
+
+	set nocount on
     Insert into tbl_Transactions(Id, PostalCode, Amount, Result, UserName, TransactionDate)
-    values (@Id, @PostalCode, @Amount, @Result, @UserName, @TransactionDate)
-END 
+    values (@Id, @PostalCode, @Amount, @Result, @UserName, @TransactionDate)')
+END
+GO
---
+++ Payspace.Test.Project.IntegrationTests/DbTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Logging.Abstractions;
+using Payspace.Test.Project.Handlers;
+using Payspace.Test.Project.Models;
+
+namespace Payspace.Test.Project.IntegrationTests;
+
+public class DbTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void TestingLiteDb()
+    {
+        var logger = new NullLogger<LiteDbHandler>();
+        var liteDbHandler = new LiteDbHandler(logger);
+         liteDbHandler.SaveRequest(
+            new CalculateTransactions(new CalculateRequest() { Amount = 7000, PostalCode = "7000" }, 7000, "Test User"));
+        var rec = liteDbHandler.GetRecordsRequest("Test User");
+        Assert.That(rec, Has.Count.EqualTo(1));
+        var res = liteDbHandler.DeleteRecord(rec.First().Id.ToString());
+        Assert.That(res, Is.True);
+    }
+    [Test]
+    public void TestingSqlDb()
+    {
+        var logger = new NullLogger<SqlDbHandler>();
+        var liteDbHandler = new SqlDbHandler(logger);
+        liteDbHandler.SaveRequest(
+            new CalculateTransactions(new CalculateRequest() { Amount = 7000, PostalCode = "7000" }, 7000, "Test User"));
+        var rec = liteDbHandler.GetRecordsRequest("Test User");
+        Assert.That(rec, Has.Count.EqualTo(1));
+        var res = liteDbHandler.DeleteRecord(rec.First().Id.ToString());
+        Assert.That(res, Is.True);
+    }
+}
